85c5780dda2bb4bd51bf332b1421b47b89b10131
Adekomi AbdulBaaqi <adekomiabdulbaqi3@gmail.com>
2020-12-22 07:54:18 +0100

    okay

--- app/src/main/java/com/example/tabbedactivity/ui/main/SectionsPagerAdapter.java
+++ app/src/main/java/com/example/tabbedactivity/ui/main/SectionsPagerAdapter.java
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 
+import com.example.tabbedactivity.Fragment1;
 import com.example.tabbedactivity.R;
 
 /**
@@ -17,7 +18,7 @@ import com.example.tabbedactivity.R;
 public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
   @StringRes
-  private static final int[] TAB_TITLES = new int[]{R.string.tab_text_1, R.string.tab_text_2};
+  private static final int[] TAB_TITLES = new int[]{R.string.tab_text_1, R.string.tab_text_2, R.string.tab_text_3};
   private final Context mContext;
 
   public SectionsPagerAdapter(Context context, FragmentManager fm) {
@@ -27,9 +28,16 @@ public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
   @Override
   public Fragment getItem(int position) {
-    // getItem is called to instantiate the fragment for the given page.
-    // Return a PlaceholderFragment (defined as a static inner class below).
-    return PlaceholderFragment.newInstance(position + 1);
+    Fragment fragment = null;
+    switch (position){
+      case 0: fragment = new Fragment1();
+        break;
+      case 1: fragment = new Fragment2();
+        break;
+      case 2: fragment = new Fragment3();
+        break;
+    }
+    return fragment;
   }
 
   @Nullable
@@ -41,6 +49,6 @@ public class SectionsPagerAdapter extends FragmentPagerAdapter {
   @Override
   public int getCount() {
     // Show 2 total pages.
-    return 2;
+    return 3;
   }
 }
